2a96d715020cf8d2f50c635aa616c6259de1367f
unknown <ajipunk008@gmail.com>
2022-03-10 09:31:39 +0700

    initial commit

---
+++ config/env/production/database.js
@@ -0,0 +1,23 @@
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        schema: env("DATABASE_SCHEMA", "public"), // Not required
+        ssl: {
+          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        },
+      },
+      debug: false,
+    },
+  };
+};
